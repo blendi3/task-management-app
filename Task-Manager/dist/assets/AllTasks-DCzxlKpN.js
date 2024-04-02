7c92c72ab3e287934732e5f7334dfb9149fc5e1d
Blendi Ivanja <blendiivanja2@gmail.com>
2024-04-02 17:37:46 +0200

    removed dist from gitignore

---
+++ Task-Manager/dist/assets/AllTasks-DCzxlKpN.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-CwBlikGn.js";import{s as a,T as r}from"./AllTasks.module-B4OLY8v3.js";import"./index-B8UKdv5Z.js";import"./pickAttrs-aR3ea02b.js";const m=()=>s.jsx("div",{className:a.cardContainer,children:s.jsx("div",{className:a.cardsLayout,children:s.jsx(r,{})})});export{m as default};
---
+++ Task-Manager/dist/assets/IncompletedTasks-CES8taPT.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-CwBlikGn.js";import{s as t,T as r}from"./AllTasks.module-B4OLY8v3.js";import"./index-B8UKdv5Z.js";import"./pickAttrs-aR3ea02b.js";const m=()=>s.jsx("div",{className:t.cardContainer,children:s.jsx("div",{className:t.cardsLayout,children:s.jsx(r,{showOnlyIncompleted:!0})})});export{m as default};
